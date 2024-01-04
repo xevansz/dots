658030f40a95972515e5abcb2dada9057f82fd90
xevansz <minithbmatthew@gmail.com>
2024-01-05 02:06:36 +0530

    remove scrollbar

--- .config/nvim/lua/zen/plugins/theme.lua
+++ .config/nvim/lua/zen/plugins/theme.lua
@@ -7,5 +7,5 @@ require("catppuccin").setup({
 })
 
 vim.cmd [[colorscheme catppuccin-mocha]]
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
