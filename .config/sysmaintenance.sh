7aabed49282dc5bc241cf055f9587ea8022e9e49
xevansz <minithbmatthew@gmail.com>
2025-03-24 04:07:13 +0530

    system maintenance

--- .config/sysmaintenance.sh
+++ .config/sysmaintenance.sh
@@ -6,17 +6,15 @@ echo "Clearing pacman cache"
 pacman_cache_space_used="$(du -sh /var/cache/pacman/pkg/)"
 paccache -r 
 echo "Space saved: $pacman_cache_space_used" 
-
+echo " "
 echo "Removing orphan packages"
 yay -Qdtq | yay -Rns -
-
+echo " "
 echo "Clearing ~/.cache"
 home_cache_used="$(du -sh ~/.cache)"
 sudo rm -rf ~/.cache/
 echo "Spaced saved: $home_cache_used"
-
+echo " "
 echo "Clearing system logs"
-sudo ournalctl --vacuum-time=7d
-
-
+sudo journalctl --vacuum-time=7d
 
