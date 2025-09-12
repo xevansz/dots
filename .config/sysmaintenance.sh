46f1e3df0c4812d978334c2cae5537d5702b45a3
xevansz <minithbmatthew@gmail.com>
2025-09-12 11:40:06 +0530

    update maintenance script

--- .config/sysmaintenance.sh
+++ .config/sysmaintenance.sh
@@ -1,20 +1,38 @@
 # This code is almost entirely taken from Mr. Cejas's blog: https://fernandocejas.com/blog/engineering/2022-03-30-arch-linux-system-maintance/
-echo "Updating system"
-yay -Syu
 
-echo "Clearing pacman cache"
-pacman_cache_space_used="$(du -sh /var/cache/pacman/pkg/)"
-paccache -r 
-echo "Space saved: $pacman_cache_space_used" 
-echo " "
+#!/bin/bash
+set -e
+
+echo "Updating system"
+yay -Syu --noconfirm
+
+echo
+echo " Clearing pacman cache"
+before=$(du -sb /var/cache/pacman/pkg | cut -f1)
+paccache -rk1
+after=$(du -sb /var/cache/pacman/pkg | cut -f1)
+echo "Freed $(( (before-after)/1024/1024 )) MB"
+
+echo
 echo "Removing orphan packages"
-yay -Qdtq | yay -Rns -
-echo " "
+orphans=$(yay -Qdtq || true)
+if [ -n "$orphans" ]; then
+    yay -Rns --noconfirm $orphans
+else
+    echo "No orphan packages found."
+fi
+
+echo
 echo "Clearing ~/.cache"
-home_cache_used="$(du -sh ~/.cache)"
-sudo rm -rf ~/.cache/
-echo "Spaced saved: $home_cache_used"
-echo " "
-echo "Clearing system logs"
+before=$(du -sb ~/.cache | cut -f1)
+rm -rf ~/.cache/*
+after=$(du -sb ~/.cache | cut -f1)
+echo "Freed $(( (before-after)/1024/1024 )) MB"
+
+echo
+echo "Cleaning system logs"
 sudo journalctl --vacuum-time=7d
+
+echo
+echo "Cleanup complete "
 
