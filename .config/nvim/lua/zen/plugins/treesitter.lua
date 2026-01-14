5a284c4d4ef1848741e1400524350a73100db2af
xevansz <minithbmatthew@gmail.com>
2026-01-14 16:33:49 +0530

    fix: treesitter error

--- .config/nvim/lua/zen/plugins/treesitter.lua
+++ .config/nvim/lua/zen/plugins/treesitter.lua
@@ -1,6 +1,14 @@
-require('nvim-treesitter.configs').setup {
+-- 1. Safe Load Mechanism
+-- We try to import the configs. If it fails (status is false), we simply return.
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
+
+-- 2. Your Configuration
+configs.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "python", "javascript", "typescript", "bash", "c", "lua", "html", "json" },
+    ensure_installed = "all",
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -10,7 +18,13 @@ require('nvim-treesitter.configs').setup {
 
     highlight = {
         enable = true,
+        -- Setting this to true will run syntax highlighting even if the parser isn't installed, 
+        -- which can be slow. Keep it true only if you want TreeSitter highlighting.
+        additional_vim_regex_highlighting = false, 
     },
+    
+    -- Note: This 'autopairs' section usually requires the "nvim-autopairs" plugin 
+    -- to be installed separately to work effectively.
     autopairs = {
         enable = true,
     },
