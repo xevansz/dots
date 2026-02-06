e5aec308b0f503c2dbe8bde3b8a5d3289d60af51
xevansz <minithbmatthew@gmail.com>
2026-02-06 20:26:10 +0530

    nvim: massive theme overhaul

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,8 +4,7 @@ require('zen.keybinds')
 -- require('pywal').setup()
 
 -- Add colorscheme directory to runtimepath so colors/ files are found
-vim.opt.rtp:append(vim.fn.stdpath("config") .. "/lua/zen/colorscheme")
+-- vim.opt.rtp:append(vim.fn.stdpath("config") .. "/lua/zen/colorscheme")
 
 -- Set the colorscheme (this will load colors/compline.lua which calls setup)
 -- vim.cmd("colorscheme compline")
-vim.cmd("colorscheme kanagawa")
--- .config/nvim/lua/zen/keybinds.lua
+++ .config/nvim/lua/zen/keybinds.lua
@@ -1,35 +1,75 @@
 -- exit to file tree
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+keymap("x", "<leader>p", [["_dP]])
 
 -- yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+keymap({ "n", "v" }, "<leader>y", [["+y]])
+keymap("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+keymap("n", "Q", "<nop>")
+keymap("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<Left>", "<nop>")
-vim.keymap.set("n", "<Right>", "<nop>")
-vim.keymap.set("n", "<Up>", "<nop>")
-vim.keymap.set("n", "<Down>", "<nop>")
+keymap("n", "<Left>", "<nop>")
+keymap("n", "<Right>", "<nop>")
+keymap("n", "<Up>", "<nop>")
+keymap("n", "<Down>", "<nop>")
+
+-- Move lines (Alt+j / Alt+k)
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Swap w behavior (previous word start)
+keymap("n", "w", "b", opts)
+
+-- Visual/select extension equivalents
+keymap("v", "<Tab>", ">", opts)
+keymap("v", "<S-Tab>", "<", opts)
+
+-- yazi
+keymap("n", "<leader>E", function()
+  local tmp = "/tmp/unique-file"
+  os.execute("rm -f " .. tmp)
+  os.execute("yazi " .. vim.fn.expand("%") .. " --chooser-file=" .. tmp)
+  local f = io.open(tmp)
+  if f then
+    local file = f:read("*l")
+    f:close()
+    if file then vim.cmd("edit " .. file) end
+  end
+end, opts)
+
+-- git blame 
+keymap("n", "<leader><leader>b", function()
+  local line = vim.fn.line(".")
+  local file = vim.fn.expand("%")
+  vim.cmd("!" ..
+    "git blame -L " .. line .. "," .. line ..
+    ' --date=relative -- "' .. file .. '"')
+end, opts)
